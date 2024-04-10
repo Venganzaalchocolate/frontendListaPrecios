38ac2bc437cd9cb730486cf54f65419d26785bd0
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-04-10 10:33:00 +0200

    vista de pisos

--- src/components/Listaprecios.js
+++ src/components/Listaprecios.js
@@ -1,9 +1,109 @@
 import { useState, useEffect } from "react";
 
-export default function Listaprecios({pis}) {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Listaprecios({ pis }) {
+  if(pis!=null && pis!=[]) {
+    return (
+      <div>
+        {pis.map((casa) => {
+          return (
+            <div class='tarjeta'>
+              <div class='textoPrincipal'>
+                <label>VIVIENDA</label>
+                <label>PRECIO</label>
+                <p class='nombre'>{casa.nombrePropiedad}</p>
+                <p class='precio'>{casa.precioPropiedad} €</p>
+              </div>
+              <div class='datos'>
+                {(casa.bloquePropiedad != null) ?
+                  <div>
+                    <label>Bloque: {casa.bloquePropiedad}</label>
+                  </div>
+                  : null
+                }
+                {(casa.portalPropiedad != null) ?
+                  <div>
+                    <label>Portal: {casa.portalPropiedad}</label>
+                  </div>
+                  : null
+                }
+                {(casa.planta != null) ?
+                  <div>
+                    <label>Planta: {casa.plantaPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.puerta != null) ?
+                  <div>
+                    <label>Puerta: {casa.puertaPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.habitacionesPropiedad != null) ?
+                  <div>
+                    <label>Habitaciones: {casa.habitacionesPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.banosPropiedad != null) ?
+                  <div>
+                    <label>Baños: {casa.banosPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.metrosPropiedad != null) ?
+                  <div>
+                    <label>Contruidos: {casa.metrosPropiedad} m<sup>2</sup></label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.metrosUtilesPropiedad != null) ?
+                  <div>
+                    <label>Útiles: {casa.metrosUtilesPropiedad} m<sup>2</sup></label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.jardinPropiedad != null) ?
+                  <div>
+                    <label>Jardin: {casa.jardinPropiedad} m<sup>2</sup></label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.orientacionPropiedad != null) ?
+                  <div>
+                    <label>Orientacion: {casa.orientacionPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.trasteroPropiedad != null) ?
+                  <div>
+                    <label>Trastero nº: {casa.trasteroPropiedad}</label>
+                  </div>
+                  : null
+                }
+  
+                {(casa.plazaPropiedad != null) ?
+                  <div>
+                    <label>Plaza nº: {casa.plazaPropiedad}</label>
+                  </div>
+                  : null
+                }
+              </div>
+            </div>
+          )
+  
+        })}
+      </div>
+    )
+  } else {
+    null
+  }
+  
 }
