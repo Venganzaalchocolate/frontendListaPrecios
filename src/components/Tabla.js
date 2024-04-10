47de5fa2f913f8c2edab1235c338b258a00534db
victoria-sampalo <victoriasampalo19@gmail.com>
2024-04-10 12:28:36 +0200

    Tabla y constructor

---
+++ src/components/Tabla.js
@@ -0,0 +1,56 @@
+import React from "react";
+import datos from "../../public/datos.json";
+
+//Función que recibe un id y renderiza una tabla con datos extraidos de un json
+export default function Tabla({ idCampania }) {
+  // console.log("idCampania: "+ idCampania)
+  // console.log("Lectura datos")
+  // console.log(datos);
+  //Filtramos los datos que nos llegan para quedarnos con el registro que coincide con el idCampania
+  const campania = datos.find((campania) => campania.idCampania === idCampania);
+  // console.log(campania)
+  //propuesta de mejora, solo if(campania),
+  if (
+    idCampania != null &&
+    idCampania != "" &&
+    campania != null &&
+    campania != ""
+  ) {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="5">Datos - {campania.nombreCampania}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <tr>
+                            <th>ID Campaña:</th>
+                            <td>{campania.idCampania}</td>
+                        </tr>
+                        <tr>
+                            <th>Nombre Campaña:</th>
+                            <td>{campania.nombreCampania}</td>
+                        </tr> */}
+            <tr>
+              <th>Dato 1:</th>
+              <td>{campania.dato1}</td>
+            </tr>
+            <tr>
+              <th>Dato 2:</th>
+              <td>{campania.dato2}</td>
+            </tr>
+            <tr>
+              <th>Dato 3:</th>
+              <td>{campania.dato3}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    null;
+    console.log("No hay tabla");
+  }
+}
