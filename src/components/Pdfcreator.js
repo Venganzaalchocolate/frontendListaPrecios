39f7907c98ffb014d9a24de54bdd3598974bf72a
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-04-11 17:42:17 +0200

    cambiando pdf

--- src/components/Pdfcreator.js
+++ src/components/Pdfcreator.js
@@ -4,14 +4,7 @@ import datos from "../../public/datos.json";
 
 
 //Función para check si exxiste
-function imagenExiste(imagen) {
- try {
-     require(`../../public${imagen}`);
-     return true;
- } catch (error) {
-     return false;
- }
-}
+
 
 const styles = StyleSheet.create({
     page: {
@@ -33,25 +26,72 @@ const styles = StyleSheet.create({
         borderBottom: '1px solid black',
         paddingBottom: '10px',
     },
+
+    cajafoto: {
+        display: 'flex',
+        maxHeight: '100px'
+
+    },
     texto: {
         textAlign: 'left',
         paddingLeft: '5px',
-        
+
     },
     foto: {
-        maxHeight:'200px',
-        width:'auto'
+        objectFit: 'contain'
+    },
+    datosTitulo: {
+        fontSize:'12pt'
+    },
+    textocaja:{
+        fontSize: '8pt'
+    },
+    contenedorTabla:{
+        border: '1px solid black'
     }
-    
+
 });
 
-const Pdf = ({ pisos, cam }) => {
-    const imagenPath= `/images/${cam}.png`;
+function imagenExiste(imagen) {
+    try {
+        require(`../../public${imagen}`);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 
-    const ponerlogo=(path)=>{
-        if (imagenExiste(path)){
-            return <View>
-                <Image style={styles.foto} src={path}/>
+function ponercaja(idCampania) {
+    
+    const campania = datos.find((campania) => campania.idCampania == idCampania);
+    console.log(campania);
+    //Si existe la campaña y si existen datos en el registro json
+    if (campania && campania.contenidotabla) {
+        const contenidoTabla = campania.contenidotabla;
+        const lineasTabla = Object.keys(contenidoTabla);
+        return (
+            <View className={styles.textocaja}>
+                <Text className={styles.datosTitulo}>
+                    Datos - {campania.nombreCampania}
+                </Text>
+                <View >
+                    {/* Map que recorre, si hay dato lo saca en un div */}
+                    {lineasTabla.map((linea, index) => (
+                        <Text className={styles.texto}>{linea}: {contenidoTabla[linea]}</Text>
+                    ))}
+                </View>
+            </View>
+        );
+    }
+}
+
+const Pdf = ({ pisos, cam }) => {
+    const imagenPath = `/images/${cam}.png`;
+
+    const ponerlogo = (path) => {
+        if (imagenExiste(path)) {
+            return <View style={styles.cajafoto}>
+                <Image style={styles.foto} src={path} />
             </View>
         }
     }
@@ -61,6 +101,7 @@ const Pdf = ({ pisos, cam }) => {
             <Document>
                 <Page size="A4" style={styles.page}>
                     {ponerlogo(imagenPath)}
+                    {ponercaja(cam)}
                     {pisos.map((casa) => {
                         return (
                             <View><View style={styles.section}>
@@ -72,37 +113,37 @@ const Pdf = ({ pisos, cam }) => {
                                 {(casa.portalPropiedad != null)
                                     ? <Text style={styles.texto}>Portal: {casa.portalPropiedad}</Text>
                                     : <></>}
-                                {(casa.planta != null) 
+                                {(casa.planta != null)
                                     ? <Text style={styles.texto}>Planta: {casa.plantaPropiedad}</Text>
                                     : <></>}
-                                {(casa.puerta != null) 
+                                {(casa.puerta != null)
                                     ? <Text style={styles.texto}>Puerta: {casa.puertaPropiedad}</Text>
                                     : <></>}
                             </View><View style={styles.sectiondos}>
                                     {(casa.habitacionesPropiedad != null)
-                                    ? <Text style={styles.texto}>Habitaciones: {casa.habitacionesPropiedad}</Text>
-                                    : <></>}
+                                        ? <Text style={styles.texto}>Habitaciones: {casa.habitacionesPropiedad}</Text>
+                                        : <></>}
                                     {(casa.banosPropiedad != null)
-                                    ? <Text style={styles.texto}>Baños: {casa.banosPropiedad}</Text>
-                                    : <></>}
+                                        ? <Text style={styles.texto}>Baños: {casa.banosPropiedad}</Text>
+                                        : <></>}
                                     {(casa.metrosPropiedad != null)
-                                    ? <Text style={styles.texto}>Contruidos: {casa.metrosPropiedad} m2</Text>
-                                    : <></>}
-                                    {(casa.metrosUtilesPropiedad != null) 
-                                    ? <Text style={styles.texto}>Útiles: {casa.metrosUtilesPropiedad} m2</Text>
-                                    : <></>}
-                                    {(casa.jardinPropiedad != null) 
-                                    ?<Text style={styles.texto}>Jardin: {casa.jardinPropiedad} m2</Text>
-                                    : <></>}
-                                    {(casa.orientacionPropiedad != null) 
-                                    ? <Text style={styles.texto}>Orientacion: {casa.orientacionPropiedad}</Text>
-                                    : <></>}
-                                    {(casa.trasteroPropiedad != null) 
-                                    ? <Text style={styles.texto}>Trastero nº: {casa.trasteroPropiedad}</Text>
-                                    : <></>}
-                                    {(casa.plazaPropiedad != null) 
-                                    ? <Text style={styles.texto}>Plaza nº: {casa.plazaPropiedad}</Text>
-                                    : <></>}
+                                        ? <Text style={styles.texto}>Contruidos: {casa.metrosPropiedad} m2</Text>
+                                        : <></>}
+                                    {(casa.metrosUtilesPropiedad != null)
+                                        ? <Text style={styles.texto}>Útiles: {casa.metrosUtilesPropiedad} m2</Text>
+                                        : <></>}
+                                    {(casa.jardinPropiedad != null)
+                                        ? <Text style={styles.texto}>Jardin: {casa.jardinPropiedad} m2</Text>
+                                        : <></>}
+                                    {(casa.orientacionPropiedad != null)
+                                        ? <Text style={styles.texto}>Orientacion: {casa.orientacionPropiedad}</Text>
+                                        : <></>}
+                                    {(casa.trasteroPropiedad != null)
+                                        ? <Text style={styles.texto}>Trastero nº: {casa.trasteroPropiedad}</Text>
+                                        : <></>}
+                                    {(casa.plazaPropiedad != null)
+                                        ? <Text style={styles.texto}>Plaza nº: {casa.plazaPropiedad}</Text>
+                                        : <></>}
                                 </View></View>
                         )
 
